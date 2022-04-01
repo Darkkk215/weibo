380edb5751ef9947f89f25c1aaa7d8a45b8ec7fa
Darkkk <228243453@qq.com>
2022-04-01 16:31:58 +0800

    文件恢复

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,7 +25,13 @@ class UserController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+<<<<<<< HEAD
         session()->flash('success', '注册成功~');
+=======
+
+        Auth::login($user);
+        session()->flash('success', '注册成功~!');
+>>>>>>> 7827954 (登录时记住我2)
         return redirect()->route('users.show',[$user]);//route() 方法会自动获取 Model 的主键 等同于[$user->id]
     }
 

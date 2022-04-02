b8e02d8b0e503a1f2956f3488228d6491a8af8f7
Darkkk <228243453@qq.com>
2022-04-02 14:01:27 +0800

    移除邮件发送的 from 选项

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -96,9 +96,13 @@ class UserController extends Controller
         $name = 'Summer';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
-
+        /*
         Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
             $message->from($from, $name)->to($to)->subject($subject);
+        });
+        */
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 

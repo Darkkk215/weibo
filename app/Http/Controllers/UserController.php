e31de0b62974f9c1afbb791f2f7b1a849ba1bf85
Darkkk <228243453@qq.com>
2022-04-02 13:04:24 +0800

    发送找回密码链接

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -74,7 +74,7 @@ class UserController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => bcrypt($request->password),
+            'password' => bcrypt($request->password)
         ]);
 
         $this->sendEmailConfirmationTo($user);
--- routes/web.php
+++ routes/web.php
@@ -47,3 +47,9 @@ Route::delete('/users/{user}', 'userController@destroy')->name('users.destroy');
 Route::get('login', 'SessionsController@create')->name('login');
 Route::post('login', 'SessionsController@store')->name('login');
 Route::delete('logout', 'SessionsController@destroy')->name('logout');
+
+Route::get('password/reset',  'PasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email',  'PasswordController@sendResetLinkEmail')->name('password.email');
+
+Route::get('password/reset/{token}',  'PasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset',  'PasswordController@reset')->name('password.update');
